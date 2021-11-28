a360aa2c7f531cbefcd390c2395bdb055f965a0e
Michal Kepkowski <mkepkowski@synapse18.com>
2021-11-28 11:18:06 +1100

    String utils

--- keycloak-eKYC-plugin/keycloak-eKYC-module/src/main/java/tech/identityfirst/authenticatorforms/ConditionalVerifiedClaimsAuthenticatorFormFactory.java
+++ keycloak-eKYC-plugin/keycloak-eKYC-module/src/main/java/tech/identityfirst/authenticatorforms/ConditionalVerifiedClaimsAuthenticatorFormFactory.java
@@ -1,12 +1,9 @@
 package tech.identityfirst.authenticatorforms;
 
 import org.keycloak.Config;
-import org.keycloak.authentication.Authenticator;
-import org.keycloak.authentication.AuthenticatorFactory;
 import org.keycloak.authentication.authenticators.conditional.ConditionalAuthenticator;
 import org.keycloak.authentication.authenticators.conditional.ConditionalAuthenticatorFactory;
 import org.keycloak.models.AuthenticationExecutionModel;
-import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderConfigProperty;
 
